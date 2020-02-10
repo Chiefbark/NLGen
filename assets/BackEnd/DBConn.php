8f64724a754c519c2d0bc96d2168649ab70e0759
Chiefbark <chiefbark.dev@gmail.com>
2020-02-10 17:36:12 +0100

    Localhost connection

--- assets/BackEnd/DBConn.php
+++ assets/BackEnd/DBConn.php
@@ -1,25 +1,16 @@
 <?php
-
-
-/**
- * Patron Singleton para crear la conexion con la base de datos y en caso de tener DAOs de otras
- * clases en el futuro poder llamarla para la conexion.
- */
 class DBConn
 {
-    private static $dbhost = 'localhost';
-    private static $dbport = '27017';
+    private static $host = "172.18.0.3:27017";
+    private static $user = "root";
+    private static $password = "root";
 
     public function __construct()
-
     {
     }
-    /**
-     * Metodo Estatico que retorna una conexion con la base de datos en base a los valores restablecidos para su conexion como el host y el puerto
-     */
+
     public static function getConnection()
     {
-        $conn = new MongoDB\Client("mongodb+srv://admin:admin@cluster0-by8no.mongodb.net/test?retryWrites=true&w=majority");
-        return $conn;
+        return new MongoDB\Driver\Manager("mongodb://" . DBConn::$user . ':' . DBConn::$password . '@' . DBConn::$host);
     }
 }
