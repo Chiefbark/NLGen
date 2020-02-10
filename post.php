07e3c02ebdf77201134d8e68900bda579d44e1c3
Chiefbark <chiefbark.dev@gmail.com>
2020-02-10 19:37:44 +0100

    Webpage revamped

--- post.php
+++ post.php
@@ -1,96 +1,74 @@
+<?php
+require_once "assets/BackEnd/DBConn.php";
+require_once "assets/BackEnd/Post.php";
+require_once "assets/BackEnd/DAOPost.php";
+
+$post = new Post();
+
+if (isset($_GET['id']) && !empty($_GET['id']))
+    $post = Post::selectById($_GET['id']);
+else
+    header('location: index.php');
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <title>Post | NLGen</title>
 
-  <title>Post | NLGen</title>
-
-  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/css/post.css" rel="stylesheet">
-
-
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/styles.css" rel="stylesheet">
 </head>
 
-<body>
+<body class="d-flex flex-column">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">NLGen</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="subscribe.php">Subscribe</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container">
-      <a class="navbar-brand" href="#">Start Bootstrap</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-        aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Services</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
-          </li>
-        </ul>
-      </div>
+    <div class="container flex-grow">
+        <div class="row">
+            <div class="col-lg-12 mb-5">
+                <h1 class="mt-4"><?php echo $post->getTitle(); ?></h1>
+                <p class="lead">
+                    by <?php echo $post->getAuthor(); ?>
+                </p>
+                <hr>
+                <p>Posted on <?php echo $post->getTimeFormated(); ?></p>
+                <hr>
+                <img class="img-fluid rounded" src="<?php echo $post->getPhoto(); ?>" alt="">
+                <hr>
+                <?php echo $post->getContent(); ?>
+            </div>
+        </div>
     </div>
-  </nav>
 
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12 mb-5">
-
-        <h1 class="mt-4">Post Title</h1>
-
-        <p class="lead">
-          by
-          <a href="#">Start Bootstrap</a>
-        </p>
-        <hr>
-        <p>Posted on January 1, 2019 at 12:00 PM</p>
-        <hr>
-        <img class="img-fluid rounded" src="http://placehold.it/900x300" alt="">
-        <hr>
-        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error
-          quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus
-          inventore?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste
-          ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius
-          illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!
-        </p>
-        <blockquote class="blockquote">
-          <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-          <footer class="blockquote-footer">Someone famous in
-            <cite title="Source Title">Source Title</cite>
-          </footer>
-        </blockquote>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat
-          totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam
-          tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui,
-          necessitatibus, est!</p>
-      </div>
-    </div>
-  </div>
-
-  <footer class="py-5 bg-dark">
-    <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; NLGen 2020</p>
-    </div>
-  </footer>
-
+    <footer class="py-5 bg-dark mt-4">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; NLGen 2020</p>
+        </div>
+    </footer>
 
 </body>
 
