7bc91fd3d1183962fd53563c3674aa362d4d90bf
Chiefbark <chiefbark.dev@gmail.com>
2020-02-10 17:40:08 +0100

    updated

--- form.php
+++ form.php
@@ -1,36 +1,30 @@
 <?php
-
 require_once "assets/BackEnd/DBConn.php";
 require_once "assets/BackEnd/Post.php";
 require_once "assets/BackEnd/DAOPost.php";
-echo phpinfo();
+
 $post = new Post();
 
 if (isset($_POST) && !empty($_POST)) {
-        $post->fill($_POST['title'], $_POST['author'], $_FILES['photo'], $_POST['content']);
-        Post::insert($post);
+    $post->fill($_POST['title'], $_POST['author'], $_FILES['photo'], $_POST['content']);
+    $id = Post::insert($post);
+    header("location: post.php?id=$id");
 }
-
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <!-- Required meta tags-->
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Colorlib Templates">
-    <meta name="author" content="Colorlib">
-    <meta name="keywords" content="Colorlib Templates">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>New Post | NLGen</title>
 
     <script src="https://cdn.ckeditor.com/ckeditor5/16.0.0/classic/ckeditor.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="assets/js/form.js"></script>
-
-    <title>New Post | NLGen</title>
     <script>
-        window.addEventListener("load", function () {
+        window.addEventListener("load", function() {
             inputListener();
             ClassicEditor
                 .create(document.querySelector('#editor'), {
@@ -40,6 +34,11 @@ if (isset($_POST) && !empty($_POST)) {
                     console.error(error);
                 });
         });
+    </script>
+    <script>
+        function loadContent() {
+            $('#content').val($('.ck-content').html());
+        }
     </script>
 
     <link href="assets/css/form.css" rel="stylesheet" media="all">
@@ -53,27 +52,25 @@ if (isset($_POST) && !empty($_POST)) {
                     <h2 class="title">New Post</h2>
                 </div>
                 <div class="card-body">
-
-
-                    <form method="POST" action="form.php">
-                    <input class="" type="text" name="id" style="display: none">
+                    <form id="form" method="POST" action="form.php" onsubmit="loadContent()">
+                        <input class="" type="text" name="id" style="display: none">
                         <div class="form-row">
                             <div class="name">Title</div>
                             <div class="value">
-                                <input class="input--style-6" type="text" name="title">
+                                <input class="input--style-6" type="text" name="title" required>
                             </div>
                         </div>
                         <div class="form-row">
                             <div class="name">Author</div>
                             <div class="value">
-                                <input class="input--style-6" type="text" name="author">
+                                <input class="input--style-6" type="text" name="author" required>
                             </div>
                         </div>
                         <div class="form-row">
                             <div class="name">Upload photo</div>
                             <div class="value">
                                 <div class="input-group js-input-file">
-                                    <input class="input-file" type="file" name="photo" id="file">
+                                    <input class="input-file" type="file" name="photo" id="file" required>
                                     <label class="label--file" for="file">Choose file</label>
                                     <span class="input-file__info">No file chosen</span>
                                 </div>
@@ -81,20 +78,19 @@ if (isset($_POST) && !empty($_POST)) {
                             </div>
                         </div>
                         <div class="form-row">
-                            <textarea name="content" style="display: none;"></textarea>
+                            <textarea id="content" name="content" style="display: none;"></textarea>
                             <div class="name">Message</div>
                             <div class="value">
                                 <div id="editor">
-                                    <p>This is some sample content.</p>
+                                    <p>Write your content here.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="card-footer">
-                            <button class="btn btn--radius-2 btn--blue-2" type="submit">Submit</button>
+                            <input class="btn btn--radius-2 btn--blue-2" type="submit">
                         </div>
                     </form>
                 </div>
-
             </div>
         </div>
     </div>
