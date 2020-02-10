d7c2a1547b7f4d90edb5886534baf15e5afad54e
Chiefbark <chiefbark.dev@gmail.com>
2020-02-10 19:37:55 +0100

    Webpage revamped

--- form.php
+++ form.php
@@ -6,11 +6,10 @@ require_once "assets/BackEnd/DAOPost.php";
 $post = new Post();
 
 if (isset($_POST) && !empty($_POST)) {
-    $post->fill($_POST['title'], $_POST['author'], $_FILES['photo'], $_POST['content']);
-    $id = Post::insert($post);
+    $post->fill($_POST['title'], $_POST['author'], $_POST['content'], time());
+    $id = Post::insert($post, $_FILES['photo']);
     header("location: post.php?id=$id");
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,11 +20,8 @@ if (isset($_POST) && !empty($_POST)) {
     <title>New Post | NLGen</title>
 
     <script src="https://cdn.ckeditor.com/ckeditor5/16.0.0/classic/ckeditor.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="assets/js/form.js"></script>
     <script>
         window.addEventListener("load", function() {
-            inputListener();
             ClassicEditor
                 .create(document.querySelector('#editor'), {
                     removePlugins: ['ImageUpload']
@@ -34,66 +30,75 @@ if (isset($_POST) && !empty($_POST)) {
                     console.error(error);
                 });
         });
-    </script>
-    <script>
+
         function loadContent() {
-            $('#content').val($('.ck-content').html());
+            document.getElementById('content').value = document.querySelector('.ck-content').innerHTML;
+        }
+
+        function updateImage() {
+            document.querySelector('label.custom-file-label').innerHTML = document.getElementById('photo').files.item(0).name;
         }
     </script>
 
-    <link href="assets/css/form.css" rel="stylesheet" media="all">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/styles.css" rel="stylesheet">
 </head>
 
-<body>
-    <div class="page-wrapper bg-dark p-t-100 p-b-50">
-        <div class="wrapper wrapper--w900">
-            <div class="card card-6">
-                <div class="card-heading">
-                    <h2 class="title">New Post</h2>
-                </div>
-                <div class="card-body">
-                    <form id="form" method="POST" action="form.php" onsubmit="loadContent()">
-                        <input class="" type="text" name="id" style="display: none">
-                        <div class="form-row">
-                            <div class="name">Title</div>
-                            <div class="value">
-                                <input class="input--style-6" type="text" name="title" required>
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="name">Author</div>
-                            <div class="value">
-                                <input class="input--style-6" type="text" name="author" required>
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="name">Upload photo</div>
-                            <div class="value">
-                                <div class="input-group js-input-file">
-                                    <input class="input-file" type="file" name="photo" id="file" required>
-                                    <label class="label--file" for="file">Choose file</label>
-                                    <span class="input-file__info">No file chosen</span>
-                                </div>
-                                <div class="label--desc">Upload your photo post</div>
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <textarea id="content" name="content" style="display: none;"></textarea>
-                            <div class="name">Message</div>
-                            <div class="value">
-                                <div id="editor">
-                                    <p>Write your content here.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card-footer">
-                            <input class="btn btn--radius-2 btn--blue-2" type="submit">
-                        </div>
-                    </form>
-                </div>
+<body class="d-flex flex-column">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">NLGen</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="subscribe.php">Subscribe</a>
+                    </li>
+                </ul>
             </div>
         </div>
+    </nav>
+
+    <div class="container flex-grow">
+        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data" onsubmit="loadContent()">
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" class="form-control" name="title" id="title" required autocomplete="off">
+            </div>
+            <div class="form-group">
+                <label for="author">Author</label>
+                <input type="text" class="form-control" name="author" id="author" required autocomplete="off">
+            </div>
+            <div class="custom-file mt-3">
+                <input type="file" class="custom-file-input" name="photo" id="photo" required autocomplete="off" onchange="updateImage()">
+                <label class="custom-file-label" for="photo">Choose file</label>
+            </div>
+            <div class="form-group mt-4">
+                <textarea id="content" name="content" id="content" style="display: none;" autocomplete="off"></textarea>
+                <div class="mb-2">Content</div>
+                <div class="value">
+                    <div id="editor">
+                        <p>Write your content here.</p>
+                    </div>
+                </div>
+            </div>
+            <input type="submit" class="col-12 btn btn-dark py-3">
+        </form>
     </div>
+
+    <footer class="py-5 bg-dark mt-4">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; NLGen 2020</p>
+        </div>
+    </footer>
 
 </body>
 
