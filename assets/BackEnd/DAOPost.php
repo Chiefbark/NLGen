1d36d4ac565166075fbfdd0da52cfa8ad75892fb
Chiefbark <chiefbark.dev@gmail.com>
2020-02-10 17:35:27 +0100

    DAOPost updated

--- assets/BackEnd/DAOPost.php
+++ assets/BackEnd/DAOPost.php
@@ -3,14 +3,15 @@
 require_once 'DBConn.php';
 require_once 'Post.php';
 
-class DAOPost{
+class DAOPost
+{
     protected $conn;
 
-    
     /**
      * Constructor que establece la conexion de la clase con una instancia del singleton
      */
-    public function __construct() {
+    public function __construct()
+    {
         $this->conn = DBConn::getConnection();
     }
 
@@ -22,8 +23,47 @@ class DAOPost{
         return new DAOPost();
     }
 
+    /**
+     * Inserts a new Post into the database
+     * @param post The Post to insert
+     */
+    public function insert($post)
+    {
+        $bulk = new MongoDB\Driver\BulkWrite;
+        $temp = array(
+            'title' => $post->getTitle(),
+            'author' => $post->getAuthor(),
+            'photo' => $post->getPhoto(),
+            'content' => $post->getContent()
+        );
+        $id = $bulk->insert($temp);
+        $this->conn->executeBulkWrite('NLGen.post', $bulk);
 
-    public function getById($id)
+        return $id;
+    }
+
+    /**
+     * Selects all the Post from the database
+     * @return collection All the Posts of the database
+     */
+    public function select()
+    {
+        $query = new MongoDB\Driver\Query([]);
+        $rows = $this->conn->executeQuery("NLGen.post", $query);
+        $collection = array();
+        foreach ($rows as $row) {
+            $node = new Post($row->_id, $row->title, $row->author, $row->photo, $row->content);
+            array_push($collection, $node);
+        }
+        return $collection;
+    }
+
+    /**
+     * Selects a Post from the database
+     * @param id The id of the Post to select
+     * @return post The selected Post of the database
+     */
+    public function selectById($id)
     {
         $query = new MongoDB\Driver\Query(['_id' => new MongoDB\BSON\ObjectId($id)]);
         $rows = $this->conn->executeQuery("NLGen.post", $query);
@@ -35,25 +75,14 @@ class DAOPost{
         return $collection[0];
     }
 
-
-
-    public function insert($post)
-    {
-        $bulk = new MongoDB\Driver\BulkWrite;
-        $contacto = array(
-            'tittle' => $post->getTitle(),
-            'author' => $post->getAuthor(),
-            'photo' => $post->getPhoto(),
-            'content' => $post->getContent()
-        );
-        $bulk->insert($contacto);
-        $this->conn->executeBulkWrite('NLGen.post', $bulk);
-    }
-
+    /**
+     * Updates a Post of the database
+     * @param post The Post to update
+     */
     public function update($post)
     {
         $bulk = new MongoDB\Driver\BulkWrite;
-        $filter = ['_id' => new MongoDB\BSON\ObjectId($post->getId())]; //Toma el id del objeto recibido como parametro para el filtro
+        $filter = ['_id' => new MongoDB\BSON\ObjectId($post->getId())];
         $new = ['$set' => [
             'title' => $post->getTitle(),
             'author' => $post->getAuthor(),
@@ -64,6 +93,10 @@ class DAOPost{
         $this->conn->executeBulkWrite('NLGen.post', $bulk);
     }
 
+    /**
+     * Deletes a Post from the database
+     * @param post The Post to delete
+     */
     public function delete($post)
     {
         $bulk = new MongoDB\Driver\BulkWrite;
@@ -71,13 +104,4 @@ class DAOPost{
         $bulk->delete($filter);
         $this->conn->executeBulkWrite('NLGen.post', $bulk);
     }
-
-
-
-
-
-
-
 }
-
-?>
