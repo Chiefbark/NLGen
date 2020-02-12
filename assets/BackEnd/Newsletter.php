56dfa2d62d1d3d4ed7dafc63e2bdd7a06b370797
Chiefbark <chiefbark.dev@gmail.com>
2020-02-12 21:41:25 +0100

    Added toHTML & select funtions

--- assets/BackEnd/Newsletter.php
+++ assets/BackEnd/Newsletter.php
@@ -14,6 +14,15 @@ class NewsletterList
     }
 
     /**
+     * Selects all the Newsletters from the database
+     * @return PostList The list of the Newsletters
+     */
+    public static function select()
+    {
+        return new NewsletterList(DAONewsletter::getInstance()->select());
+    }
+
+    /**
      * Returns the list of Newsletters
      * @return list The list of Newsletters
      */
@@ -30,6 +39,18 @@ class NewsletterList
     {
         $this->list = $list;
     }
+
+    /**
+     * Returns the html template of the Newsletter
+     * @return html The html template of the Newsletter
+     */
+    public function toHTML()
+    {
+        $str  = '';
+        foreach ($this->list as $row)
+            $str .= $row->toHTML();
+        return $str;
+    }
 }
 
 class Newsletter
@@ -44,7 +65,7 @@ class Newsletter
      * @param timestamp The date when the Newsletter was created
      * @param postList The id list of Posts of the Newsletter
      */
-    public function __construct($id = '', $timestamp = '', $postList = '')
+    public function __construct($id = '', $timestamp = '', $postList = [])
     {
         $this->id = $id;
         $this->timestamp = $timestamp;
@@ -163,11 +184,35 @@ class Newsletter
         return date("F d, Y h:i:s A", $this->timestamp);
     }
 
+    /**
+     * Returns the timestamp formatted
+     * @return date The timestamp formatted
+     */
     public function getDateTime()
     {
         return date("F d, Y", $this->timestamp);
     }
 
+    /**
+     * Returns the html template of the Post
+     * @return html The html template of the Post
+     */
+    public function toHTML()
+    {
+        $str = '';
+        $str .= '<a href="newsletter.php?id=' . $this->getId() . '" class="card" id="' . $this->getId() . '">';
+        $str .= '<div class="card-body text-dark">';
+        $str .= '<h4 class="card-title">Newsletter of ' . $this->getDateTime() . '</h4>';
+        $str .= '<p class="card-text text-right"><small class="text-muted">Contains ' . sizeof($this->postList) . ' posts</small></p>';
+        $str .= '</div>';
+        $str .= '</a>';
+        return $str;
+    }
+
+    /**
+     * Returns the xml format of the Newsletter
+     * @return xml The xml format of the Newsletter
+     */
     public function toXML()
     {
         $str = '<newsletter id="' . $this->id . '" timestamp="' . $this->timestamp . '">';
@@ -179,6 +224,10 @@ class Newsletter
         return $str;
     }
 
+    /**
+     * Returns the json format of the Newsletter
+     * @return json The json format of the Newsletter
+     */
     public function toJSON()
     {
         $str = 'newsletter:{';
